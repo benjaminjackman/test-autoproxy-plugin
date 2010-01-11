4e00aabb7b947e3452fbc74e0000cc680157f6c8
Benjamin Jackman <bjackman@ash.(none)>
2010-01-11 13:09:01 -0600

    cleaned up test
    Signed-off-by: Benjamin Jackman <bjackman@ash.(none)>

--- src/test/scala/autoproxy/testing/Point3d.scala
+++ src/test/scala/autoproxy/testing/Point3d.scala
@@ -22,26 +22,29 @@ class Point2dImpl(x_ : Int, y_ : Int) extends Point2d {
 
 class Point3d(val point: Point2d, z: Int)
 
-class Point3dProxyImplExt2d(@proxy val point: Point2dImpl, val z: Int) extends Point2d {
-  def a = x
-}
-
-class Point3dProxyExtendsPoint2d(@proxy val point: Point2d, val z: Int) extends Point2d {
-  def a = x
-}
-class Point3dProxyOverrideX(@proxy val point: Point2d, val z: Int) extends Point2d {
-  override def x = 5
-  def a = x
-}
-
 class Point3dProxy(@proxy val point: Point2d, val z: Int)
 
 //TODO ERROR:
 //src/test/scala/autoproxy/testing/Point3d.scala:35: error: method y overrides nothing
 class Point3dProxyImpl(@proxy val point: Point2dImpl, val z: Int)
 
-//Scratch area:
-//case class Point3d(point: Point2d, z: Int)
-//class Point3d(@proxy val point: Point2dImpl, val z: Int) {
-//class Point3d(@proxy val point: Point2d, val z: Int) extends Point2d {
-//class Point3d(val point: Point2d, val z: Int) {
+
+class Point3dProxyExt2d(@proxy val point: Point2d, val z: Int) extends Point2d {
+  def a = x
+}
+
+class Point3dProxyImplExt2d(@proxy val point: Point2dImpl, val z: Int) extends Point2d {
+  def a = x
+}
+
+class Point3dProxyOverrideXExt2d(@proxy val point: Point2d, val z: Int) extends Point2d {
+  override def x = 5
+  def a = x
+}
+
+class Point3dProxyImplOverrideXExt2d(@proxy val point: Point2d, val z: Int) extends Point2d {
+  override def x = 5
+  def a = x
+}
+
+//TODO figure out to have negative cases, which should cause the build to fail
